90584bb1e2140077f5ef4a7175b9f136042d90e6
ja <banaszekl@wp.pl>
2014-05-12 15:47:36 +0200

    zakomentowane

--- ludiMenda.cpp
+++ ludiMenda.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main()
 {
 	cout<<"Nara";
-<<<<<<< HEAD
+//<<<<<<< HEAD
 	cout<<"Szeremi lobuzie, wsadzimy Ci cos w buzie"<<endl;
-=======
+//=======
 	for(int i=0;i<5;i++)
 	        cout << "szeremi fiutrk" << endl;
->>>>>>> 2b50cc22c97a6ae1833a8c52db465d77f9fa20ee
+//>>>>>>> 2b50cc22c97a6ae1833a8c52db465d77f9fa20ee
+system("pause");
 	return 0;
 }
